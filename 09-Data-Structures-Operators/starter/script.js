52926f568275d107f4d8b4be9263f076292609d8
hemjay07 <mujeebopabode07@gmail.com>
2022-06-02 01:25:52 +0100

    completed this section
    learnt more about strings, different method you can use on strings and so on, im really exited to move on to the next section

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -79,38 +79,46 @@
 
 // restaurant.orderPizza('pizzabread', 'meat', 'onion');
 
-git       'Alaba',
-      'Davies',
-      'Kimmich',
-      'Goretzka',
-      'Coman',
-      'Muller',
-      'Gnarby',
-      'Lewandowski',
-    ],
-    [
-      'Burki',
-      'Schulz',
-      'Hummels',
-      'Akanji',
-      'Hakimi',
-      'Weigl',
-      'Witsel',
-      'Hazard',
-      'Brandt',
-      'Sancho',
-      'Gotze',
-    ],
-  ],
-  score: '4:0',
-  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
-  date: 'Nov 9th, 2037',
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
-  },
-};
+// const game = {
+//   team1: 'Bayern Munich',
+//   team2: 'Borrussia Dortmund',
+//   players: [
+//     [
+//       'Neuer',
+//       'Pavard',
+//       'Martinez',
+//       'Alaba',
+//       'Davies',
+//       'Kimmich',
+//       'Goretzka',
+//       'Coman',
+//       'Muller',
+//       'Gnarby',
+//       'Lewandowski',
+//     ],
+//     [
+//       'Burki',
+//       'Schulz',
+//       'Hummels',
+//       'Akanji',
+//       'Hakimi',
+//       'Weigl',
+//       'Witsel',
+//       'Hazard',
+//       'Brandt',
+//       'Sancho',
+//       'Gotze',
+//     ],
+//   ],
+//   score: '4:0',
+//   scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+//   date: 'Nov 9th, 2037',
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
 // const [players1, players2] = game.players;
 // const [gk, ...fieldPlayers] = players1;
 // const allPlayers = [...players1, ...players2];
@@ -187,23 +195,23 @@ git       'Alaba',
 // }
 
 // Maps: Iteration
-const question = new Map([
-  ['question', 'What is the best programming language in the world?'],
-  [1, 'C'],
-  [2, 'Java'],
-  [3, 'JavaScript'],
-  ['correct', 3],
-  [true, 'Correct 🎉'],
-  [false, 'Try again!'],
-]);
+// const question = new Map([
+//   ['question', 'What is the best programming language in the world?'],
+//   [1, 'C'],
+//   [2, 'Java'],
+//   [3, 'JavaScript'],
+//   ['correct', 3],
+//   [true, 'Correct 🎉'],
+//   [false, 'Try again!'],
+// ]);
 
-console.log(question.get('question'));
+// console.log(question.get('question'));
 
-for (const [key, value] of question) {
-  if (typeof key === 'number') console.log(`${key} . ${value}`);
-}
+// for (const [key, value] of question) {
+//   if (typeof key === 'number') console.log(`${key} . ${value}`);
+// }
 
-let answer = Number(prompt('your answer'));
+// let answer = Number(prompt('your answer'));
 
 // console.log(
 //   `${
@@ -213,4 +221,160 @@ let answer = Number(prompt('your answer'));
 //   }`
 // );
 // Or
-console.log(question.get(question.get('correct') === answer));
+// console.log(question.get(question.get('correct') === answer));
+
+// const gameEvents = new Map([
+//   [17, '⚽️ GOAL'],
+//   [36, '🔁 Substitution'],
+//   [47, '⚽️ GOAL'],
+//   [61, '🔁 Substitution'],
+//   [64, '🔶 Yellow card'],
+//   [69, '🔴 Red card'],
+//   [70, '🔁 Substitution'],
+//   [72, '🔁 Substitution'],
+//   [76, '⚽️ GOAL'],
+//   [80, '⚽️ GOAL'],
+//   [92, '🔶 Yellow card'],
+// ]);
+// 1. Create an array 'events' of the different game events that happened (no
+//   duplicates)
+// const events = [...new Set([gameEvents.values()])];
+
+// 2. After the game has finished, is was found that the yellow card from minute 64
+// was unfair. So remove this event from the game events log.
+
+// gameEvents.delete(64);
+
+// 3. Compute and log the following string to the console: "An event happened, on
+// average, every 9 minutes" (keep in mind that a game has 90 minutes)
+
+// console.log(gameEvents.size);
+// console.log(
+//   `An event happened, on average every ${90 / gameEvents.size} minutes`
+// );
+
+// 4. Loop over 'gameEvents' and log each element to the console, marking
+// whether it's in the first half or second half (after 45 min) of the game, like this:
+// [FIRST HALF] 17: ⚽ GOAL
+// for (const [time, event] of gameEvents) {
+//   console.log(
+//     time > 45
+//       ? `[SECOND HALF] ${time} : ${event}`
+//       : `[FIRST HALF] ${time} : ${event}`
+//   );
+// }
+
+// ////////////////////////
+// working with strings
+
+// const airline = 'TAP Air Portugal';
+// const plane = 'A320';
+
+// console.log('gksi'[3]);
+// console.log(airline.slice(7, 10));
+// console.log(airline.slice(-3));
+
+// const compareEmail = function (x, y) {
+//   const comparedEmail = y.toLowerCase().trim();
+//   console.log(x === comparedEmail);
+// };
+
+// compareEmail('mujeeb', ' Mujeeb  ');
+
+// const priceGB = '234,4E';
+// const priceUS = priceGB.replace('E', '$');
+// const announcement =
+//   'all passengers should come to the boarding door 23, Boarding door 23';
+
+// console.log(announcement.replaceAll('door', 'gate'));
+
+// const capitalizeName = function (name) {
+//   const names = name.split(' ');
+//   let capitalName = [];
+//   for (const n of names) {
+//     // capitalName.push(n[0].toUpperCase() + n.slice(1));
+//     capitalName.push(n.replace(n[0], n[0].toUpperCase()));
+//   }
+//   console.log(capitalName.join(' '));
+// };
+// // capitalizeName('abdul mujeeb oladimeji opabode');
+
+// const maskCreditCard = function (number) {
+//   const str = String(number);
+//   console.log(str.slice(-4).padStart(str.length, '*'));
+// };
+// maskCreditCard(485900358839339);
+// maskCreditCard('59500394950449585');
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/* 
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+first_name
+Some_Variable 
+calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 😀
+*/
+
+// document.body.append(document.createElement('textarea'));
+// document.body.append(document.createElement('button'));
+
+// const button = document.querySelector('button');
+// button.addEventListener('click', function () {
+//   const text = document.querySelector('textarea').value;
+//   const textArray = text.split('\n');
+
+//   for ([i, t] of textArray.entries()) {
+//     let te = t.toLowerCase();
+//     let [first, second] = te.split('_');
+//     let camelCase = first + `${second[0].toUpperCase()}` + second.slice(1);
+//     console.log(camelCase.padEnd(20, ' ') + '✅'.repeat(i + 1));
+//   }
+// });
+
+// // INPUT FROM MAYBE AN API
+// const flights =
+//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// for (const flight of flights.split('+')) {
+//   // destructure the output array from the split method into 4 variables
+//   const [occurence, departure, arrival, timing] = flight.split(';');
+//   // output = replace all "_" in occurence with " ", take the first 3 letters of arrival and departure and convert them to uppercase. Join all these together in a string literal
+//   let outPut = `${occurence.replaceAll('_', ' ')} from ${departure
+//     .toUpperCase()
+//     .slice(0, 3)} to ${arrival.toUpperCase().slice(0, 3)} (${timing}) `;
+//   console.log(
+//     // pad output at the start,add 🛑 if output constains the string "delayed"
+//     outPut.includes('Delayed')
+//       ? ('🛑' + outPut).padStart(50)
+//       : outPut.padStart(50)
+//   );
+// }
+
+// // RESULT
+// //       🛑 Delayed Departure from FAO to TXL (11:25)
+// //                    Arrival from BRU to FAO (11:45)
+// //         🛑 Delayed Arrival from HEL to FAO (12:05)
+// //                  Departure from FAO to LIS (12:30)
